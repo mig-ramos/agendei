dcba773536d44926f91b6c2045860fd4350f6596
mig-ramos <mig.ramos10@gmail.com>
2024-10-16 23:31:34 -0300

    feat: cria tela main com abas

--- agendei-mobile/App.js
+++ agendei-mobile/App.js
@@ -2,11 +2,12 @@ import AbaHome from "./src/screens/abahome/abahome";
 import Account from "./src/screens/acoount/account";
 import AbaCalendar from "./src/screens/abacalendar/abacalendar";
 import AbaProfile from "./src/screens/abaprofile/abaprofile";
+import Main from "./src/screens/main/main";
 
 export default function App(props) {
   return (
     <>
-      <AbaProfile />
+      <Main />
     </>
   );
 }
