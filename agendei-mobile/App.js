6ceb01b3e9954c4cac6efeedebf6d6a5ae424e5f
mig-ramos <mig.ramos10@gmail.com>
2024-10-17 09:00:55 -0300

    feat: cria telaschedule

--- agendei-mobile/App.js
+++ agendei-mobile/App.js
@@ -4,11 +4,12 @@ import AbaCalendar from "./src/screens/abacalendar/abacalendar";
 import AbaProfile from "./src/screens/abaprofile/abaprofile";
 import Main from "./src/screens/main/main";
 import Services from "./src/screens/services/services";
+import Schedule from "./src/screens/schedule/schedule";
 
 export default function App(props) {
   return (
     <>
-      <Services />
+      <Schedule />
     </>
   );
 }
