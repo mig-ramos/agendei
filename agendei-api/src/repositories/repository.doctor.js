13e40c7b9b60b9116b1149706ff2111a267a7f2e
mig-ramos <mig.ramos10@gmail.com>
2024-10-18 23:44:47 -0300

    feat: conclui acesso ao banco

--- agendei-api/src/repositories/repository.doctor.js
+++ agendei-api/src/repositories/repository.doctor.js
@@ -1,10 +1,16 @@
+import { query } from "../database/sqlite.js";
+
 async function Listar() {
   //simula o acesso ao banco
-  const doctors = [
-    { id: 1, name: "Miguel", specialty: "Cardiologista", icon: "M" },
-    { id: 2, name: "Joãol", specialty: "Clínico Geral", icon: "M" },
-    { id: 3, name: "Maria", specialty: "Cardiologista", icon: "F" },
-  ];
+  // const doctors = [
+  //   { id: 1, name: "Miguel", specialty: "Cardiologista", icon: "M" },
+  //   { id: 2, name: "Joãol", specialty: "Clínico Geral", icon: "M" },
+  //   { id: 3, name: "Maria", specialty: "Cardiologista", icon: "F" },
+  // ];
+
+  let sql = "select * from doctors order by name";
+
+  const doctors = await query(sql, []);
 
   return doctors;
 }
