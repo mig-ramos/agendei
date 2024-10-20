66586d309359382957bfddcb969f1106e03f4ae5
mig-ramos <mig.ramos10@gmail.com>
2024-10-20 01:01:59 -0300

    feat: conclui crud da parte doctors

--- agendei-api/src/repositories/repository.doctor.js
+++ agendei-api/src/repositories/repository.doctor.js
@@ -1,6 +1,6 @@
 import { query } from "../database/sqlite.js";
 
-async function Listar() {
+async function Listar(name) {
   //simula o acesso ao banco
   // const doctors = [
   //   { id: 1, name: "Miguel", specialty: "Cardiologista", icon: "M" },
@@ -8,11 +8,52 @@ async function Listar() {
   //   { id: 3, name: "Maria", specialty: "Cardiologista", icon: "F" },
   // ];
 
-  let sql = "select * from doctors order by name";
+  let filtro = [];
 
-  const doctors = await query(sql, []);
+  let sql = "select * from doctors ";
+
+  if (name) {
+    sql = sql + "where name like ? ";
+    filtro.push("%" + name + "%");
+  }
+
+  sql = sql + "order by name";
+
+  const doctors = await query(sql, filtro);
 
   return doctors;
 }
 
-export default { Listar };
+async function Inserir(name, specialty, icon) {
+  //simula o acesso ao banco
+  // const doctors = [
+  //   { id: 1, name: "Miguel", specialty: "Cardiologista", icon: "M" },
+  //   { id: 2, name: "Joãol", specialty: "Clínico Geral", icon: "M" },
+  //   { id: 3, name: "Maria", specialty: "Cardiologista", icon: "F" },
+  // ];
+
+  let sql = `INSERT INTO doctors (name, specialty, icon) values(?, ?, ?)
+  returning id_doctor`;
+
+  const doctor = await query(sql, [name, specialty, icon]);
+
+  return doctor[0];
+}
+
+async function Editar(id_doctor, name, specialty, icon) {
+  let sql = `UPDATE  doctors SET name= ?, specialty= ?, icon= ? WHERE id_doctor = ?`;
+
+  await query(sql, [name, specialty, icon, id_doctor]);
+
+  return { id_doctor };
+}
+
+async function Excluir(id_doctor) {
+  let sql = `DELETE from doctors WHERE id_doctor = ?`;
+
+  await query(sql, [id_doctor]);
+
+  return { id_doctor };
+}
+
+export default { Listar, Inserir, Editar, Excluir };
--- agendei-api/src/services/service.doctor.js
+++ agendei-api/src/services/service.doctor.js
@@ -1,9 +1,27 @@
 import repoDoctor from "../repositories/repository.doctor.js";
 
-async function Listar() {
-  const doctors = await repoDoctor.Listar();
+async function Listar(name) {
+  const doctors = await repoDoctor.Listar(name);
 
   return doctors;
 }
 
-export default { Listar };
+async function Inserir(name, specialty, icon) {
+  const doctor = await repoDoctor.Inserir(name, specialty, icon);
+
+  return doctor;
+}
+
+async function Editar(id_doctor, name, specialty, icon) {
+  const doctor = await repoDoctor.Editar(id_doctor, name, specialty, icon);
+
+  return doctor;
+}
+
+async function Excluir(id_doctor) {
+  const doctor = await repoDoctor.Excluir(id_doctor);
+
+  return doctor;
+}
+
+export default { Listar, Inserir, Editar, Excluir };
