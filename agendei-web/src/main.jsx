8686da9fc4fb036551ec113749e420fc8df501e5
mig-ramos <mig.ramos10@gmail.com>
2024-10-23 08:21:37 -0300

    feat: cria tela register com estilo

--- agendei-web/src/main.jsx
+++ agendei-web/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import Login from './pages/login/login.jsx'
+// import Login from './pages/login/login.jsx'
+import Register from './pages/register/register.jsx';
 import "./styles/global.css";
 
 createRoot(document.getElementById('root')).render(
-     <Login />
+     <Register />
 )
