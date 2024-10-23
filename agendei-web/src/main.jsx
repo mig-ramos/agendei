bc00fa4c7c2ce3d1ed071821bd426323a9c2c183
mig-ramos <mig.ramos10@gmail.com>
2024-10-22 22:38:59 -0300

    feat: instala bootstrap e font poppins

--- agendei-web/src/main.jsx
+++ agendei-web/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import Login from './pages/login/login.jsx'
-
+import "./styles/global.css";
 
 createRoot(document.getElementById('root')).render(
      <Login />
