4a4a220f02f0044f722748653d292f417db36025
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-10-25 16:31:11 -0300

    feat: cria lista e editar por id_appointment

--- agendei-api/src/repositories/repository.user.js
+++ agendei-api/src/repositories/repository.user.js
@@ -1,47 +1,64 @@
 import { query } from "../database/sqlite.js";
 
 async function Inserir(name, email, password) {
-  let sql = `INSERT INTO users (name, email, password) values(?, ?, ?)
-  returning id_user`;
 
-  const user = await query(sql, [name, email, password]);
+    let sql = `insert into users(name, email, password) values(?, ?, ?)
+    returning id_user`;
 
-  return user[0];
+    const user = await query(sql, [name, email, password]);
+
+    return user[0];
 }
 
 async function ListarByEmail(email) {
-  let sql = `select * from users where email = ?`;
 
-  const user = await query(sql, [email]);
+    let sql = `select * from users where email = ?`;
 
-  if (user.length == 0) return [];
-  else return user[0];
+    const user = await query(sql, [email]);
+
+    if (user.length == 0)
+        return [];
+    else
+        return user[0];
 }
 
 async function Profile(id_user) {
-  let sql = `select id_user, name, email from users where id_user = ?`;
 
-  const user = await query(sql, [id_user]);
+    let sql = `select id_user, name, email from users where id_user = ?`;
 
-  return user[0];
+    const user = await query(sql, [id_user]);
+
+    return user[0];
 }
 
 async function InserirAdmin(name, email, password) {
-  let sql = `INSERT INTO admins (name, email, password) values(?, ?, ?)
-  returning id_admin`;
 
-  const user = await query(sql, [name, email, password]);
+    let sql = `insert into admins(name, email, password) values(?, ?, ?)
+    returning id_admin`;
 
-  return user[0];
+    const user = await query(sql, [name, email, password]);
+
+    return user[0];
 }
 
 async function ListarByEmailAdmin(email) {
-  let sql = `select * from admins where email = ?`;
 
-  const user = await query(sql, [email]);
+    let sql = `select * from admins where email = ?`;
 
-  if (user.length == 0) return [];
-  else return user[0];
+    const user = await query(sql, [email]);
+
+    if (user.length == 0)
+        return [];
+    else
+        return user[0];
 }
 
-export default { Inserir, ListarByEmail, Profile, InserirAdmin, ListarByEmailAdmin };
+async function Listar() {
+
+    let sql = `select id_user, name, email from users order by name`;
+
+    const users = await query(sql, []);
+    return users;
+}
+
+export default { Inserir, ListarByEmail, Profile, InserirAdmin, ListarByEmailAdmin, Listar }
--- agendei-api/src/services/service.user.js
+++ agendei-api/src/services/service.user.js
@@ -3,61 +3,79 @@ import repoUser from "../repositories/repository.user.js";
 import jwt from "../token.js";
 
 async function Inserir(name, email, password) {
-  const hashPassword = await bcrypt.hash(password, 10);
-  const user = await repoUser.Inserir(name, email, hashPassword);
 
-  user.token = jwt.CreateToken(user.id_user);
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await repoUser.Inserir(name, email, hashPassword);
 
-  return user;
+    user.token = jwt.CreateToken(user.id_user);
+
+    return user;
 }
 
 async function Login(email, password) {
-  const user = await repoUser.ListarByEmail(email);
 
-  if (user.length == 0) return [];
-  else {
-    if (await bcrypt.compare(password, user.password)) {
-      delete user.password;
+    const user = await repoUser.ListarByEmail(email);
 
-      user.token = jwt.CreateToken(user.id_user);
+    if (user.length == 0)
+        return [];
+    else {
+        if (await bcrypt.compare(password, user.password)) {
+            delete user.password;
 
-      return user;
-    } else return [];
-  }
+            user.token = jwt.CreateToken(user.id_user);
 
-  return user;
+            return user;
+        } else
+            return [];
+    }
+
+    return user;
 }
 
 async function Profile(id_user) {
-  const user = await repoUser.Profile(id_user);
 
-  return user;
+    const user = await repoUser.Profile(id_user);
+
+    return user;
 }
 
 async function InserirAdmin(name, email, password) {
-  const hashPassword = await bcrypt.hash(password, 10);
-  const user = await repoUser.InserirAdmin(name, email, hashPassword);
 
-  user.token = jwt.CreateToken(user.id_user);
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await repoUser.InserirAdmin(name, email, hashPassword);
 
-  return user;
+    user.token = jwt.CreateToken(user.id_user);
+
+    return user;
 }
 
 async function LoginAdmin(email, password) {
-  const user = await repoUser.ListarByEmailAdmin(email);
 
-  if (user.length == 0) return [];
-  else {
-    if (await bcrypt.compare(password, user.password)) {
-      delete user.password;
+    const user = await repoUser.ListarByEmailAdmin(email);
 
-      user.token = jwt.CreateToken(user.id_user);
+    if (user.length == 0)
+        return [];
+    else {
+        if (await bcrypt.compare(password, user.password)) {
+            delete user.password;
 
-      return user;
-    } else return [];
-  }
+            user.token = jwt.CreateToken(user.id_user);
 
-  return user;
+            return user;
+        } else
+            return [];
+    }
+
+    return user;
 }
 
-export default { Inserir, Login, Profile, InserirAdmin, LoginAdmin };
+async function Listar() {
+
+    const users = await repoUser.Listar();
+
+    return users;
+}
+
+
+
+export default { Inserir, Login, Profile, InserirAdmin, LoginAdmin, Listar }
